20fd74123568ebb7128744da928df3ae6b4590a7
Han Kwan <saihankwansaing@gmail.com>
2022-03-31 21:04:05 +0630

    can't connect to localhost

--- restudy.php
+++ restudy.php
@@ -1,44 +1,42 @@
+<?php
+    $pdo = new PDO('mysql:host=localhost;port=3306;dbname=product_crud', 'root', '');
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo 'hello';
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Restudy php with git</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <title>Product crud</title>
 </head>
 <body>
-    <?php
-        function sumAll(...$num) {
-            $sum = 0;
-            foreach ($num as $N) {
-                $sum += $N;
-            }
-            echo $sum;
-        }
-        echo sumAll (1, 2, 3, 4, 5, 6);
-
-        $role = "thief";
-        switch($role) {
-            case "manager": echo "manager";
-            break;
-            case "CEO": echo "CEO";
-            break;
-            default: echo "not found";
-        }
-
-        $person = [
-            "name" => "john",
-            "age" => 23,
-            "likes" => ["coffee", "burger"]
-        ];
-        foreach ($person as $p) {
-            if (is_array($p)) {
-                return implode(",", $p);
-            } else {
-                return $p;
-            }
-        };
-        echo $person["likes"]; //cant figure out
-    ?>
+    <h1>Product crud</h1>
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">title</th>
+      <th scope="col">description</th>
+      <th scope="col">image</th>
+      <th scope="col">prize</th>
+      <th scope="col">create_date</th>
+    </tr>
+  </thead>
+  <tbody>
+      <?php 
+      
+      ?>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+  </tbody>
+</table>
 </body>
 </html>
