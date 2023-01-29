08454d6ec8f4f0321c0d030a4231cea5622b80ab
Han Kwan <saihankwansaing@gmail.com>
2023-01-29 11:03:43 +0630

    messed with placing echo

--- dontBeLazyAndStudy/study.php
+++ dontBeLazyAndStudy/study.php
@@ -4,39 +4,45 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>After a long time</title>
+    <title>Restudying</title>
 </head>
 <body>
+    <h4>simple calculator</h4>
     <form action="study.php" method="post">
         <input type="number" name="num1">
         <input type="text" name="opt">
         <input type="number" name="num2">
         <button type="submit">=</button>
     </form>
-
+    <?php 
+        $num1 = $_POST['num1'];
+        $opt = $_POST['opt'];
+        $num2 = $_POST['num2'];
+        $ans = 0;
+        if ($opt === "+") {
+            $ans = $num1 + $num2; 
+        } else if ($opt === "-") {
+            $ans = $num1 - $num2; 
+        } else if ($opt === "*") {
+            $ans = $num1 * $num2; 
+        } else if ($opt === "/") {
+            $ans = $num1 / $num2; 
+        };
+        ?>
+        <div><?php echo $ans ?></div>
+    
     <form action="study.php" method="post">
         <h4>associative arr</h4>
         <input type="text" name="wName" value="<?php echo $_POST['wName'] ?>">
         <button type="submit">Search</button>
     </form>
-
+    
     <?php
-        $num1 = $_POST['num1'];
-        $opt = $_POST['opt'];
-        $num2 = $_POST['num2'];
-        if ($opt === "+") {
-            echo $num1 + $num2; 
-        } else if ($opt === "-") {
-            echo $num1 - $num2; 
-        } else if ($opt === "*") {
-            echo $num1 * $num2; 
-        } else if ($opt === "/") {
-            echo $num1 / $num2; 
-        };
-
         $workers = ['johnny' => '120k', 'mary' => '110k'];
         $toLower = strtolower($_POST['wName']);
         echo $workers[$toLower];
     ?>
+    
+
 </body>
 </html>
