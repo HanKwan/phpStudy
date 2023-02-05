3745d219690c20d689bf3fb44b14da9e43599d19
Han Kwan <saihankwansaing@gmail.com>
2023-02-05 18:51:08 +0630

    made note form

--- dontBeLazyAndStudy/note/index.php
+++ dontBeLazyAndStudy/note/index.php
@@ -7,7 +7,15 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <title>Document</title>
 </head>
-<body>
-        
+<body class="d-flex justify-content-center">
+    <div class="container mt-5 row justify-content-center">
+        <div class="col-4">
+            <form action="" method="post" class="d-flex flex-column align-items-center">
+                <input type="text" name="title" class="mb-2 w-100" placeholder="Note title">
+                <textarea name="body" class="mb-3 w-100" cols="10" rows="3" placeholder="About"></textarea>
+                <button class="btn-sm btn btn-primary w-50">Add note</button>
+            </form> 
+        </div>
+    </div>
 </body>
 </html>
